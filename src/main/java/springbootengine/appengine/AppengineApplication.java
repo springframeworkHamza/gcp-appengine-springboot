fbf25c2d6181fb4de166426338a54e0784644afc
Aouina Chaoui <hamza.aouina-chaoui@capgemini.com>
2021-10-27 11:35:34 +0200

    Initial commit

---
+++ src/main/java/springbootengine/appengine/AppengineApplication.java
@@ -0,0 +1,13 @@
+package springbootengine.appengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppengineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springbootengine/appengine/AppengineApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootengine.appengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppengineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
